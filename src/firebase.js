0e8270d607338aef3ffe4b55dc8d85752e70691b
JohnDaisoN <100117753+JohnDaisoN@users.noreply.github.com>
2023-09-23 11:41:39 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaRUpjz_7b_P0JIVzs25p4G__j2c0jE8Q",
+  authDomain: "thrive-urban-9d119.firebaseapp.com",
+  projectId: "thrive-urban-9d119",
+  storageBucket: "thrive-urban-9d119.appspot.com",
+  messagingSenderId: "432231047452",
+  appId: "1:432231047452:web:a32a7203d1fd75adf4dad9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
